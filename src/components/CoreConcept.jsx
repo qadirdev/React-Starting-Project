29c45a27914c7ab7d820f028fbb1a0edbfb1b5f2
Qadir <qadir.qanbarpoor@gmail.com>
2024-04-27 07:27:15 -0700

    0

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,10 @@
+const CoreConcept = ({ title, description, image }) => {
+    return (
+      <li>
+        <img src={image} alt="..." />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  };
+export default CoreConcept
